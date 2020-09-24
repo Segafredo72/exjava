e0bd4a06e52e9924beac249750af4feba84d96a6
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-09-24 10:34:19 +0200

    Add fixed version of exA2.java

--- exA2/exA2.java
+++ exA2/exA2.java
@@ -8,17 +8,23 @@ public static void main(String[] args){
 	String title;
 	Integer numberOfDays;
 	Double pricePerDay;
+	Boolean priorKnowledgeRequired;
 	List<String> instructors = new ArrayList<String>();
 	
 	title = "Java Reskilling";
 	numberOfDays = 25;
 	pricePerDay = 95.50;
+	priorKnowledgeRequired = false;
 	
 	instructors.add("Sandy");
 	instructors.add("Kris");
 		
 	System.out.println("Course "+ title +" is "+ numberOfDays +" days long at day rate "+ pricePerDay + " EUR" );
-
+	if (priorKnowledgeRequired) {
+		System.out.println("Prior knowledge is requireed !");
+	}else{
+		System.out.println("Prior knowledge is not requireed !");
+	}
 	System.out.println("Number of instructors is "+ instructors.size());
 	for(String instructor: instructors){ 
 		System.out.println("Instructors are : " + instructor);
